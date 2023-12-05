721ca6e856ed1202ea7c4680ba18aac70b156dd3
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-05 17:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const browserInfo = document.getELementById('browser-info');
+
+function displayBrowserInfo(){
+ const browesrName = navigator.appName;
+const browserVersion = navigator.appVersion;
+
+const infoMessage = document.createTextNode( "You are using " + browserName + " version " + browserVersionersion.);
+browserInfo.innerHTML = '';
+browserInfo.appendChild(infoMessage);
+}
+displayBrowserInfo();
